6793e3afbbf0621310b4ef79c3e255d03a1c690c
Julian Maldonado <jmaldo6@outlook.com>
2025-08-26 21:53:21 -0500

    Add project to create user to test

---
+++ Million.TestUserCreator/IdentitySeeder.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Million.Domain.Entities;
+
+namespace Million.TestUserCreator
+{
+    public class IdentitySeeder
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public IdentitySeeder(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task SeedAsync()
+        {
+            const string username = "testuser";
+            const string email = "test@example.com";
+            const string password = "Test1234!";
+            const string role = "User";
+
+            if (!await _roleManager.RoleExistsAsync(role))
+                await _roleManager.CreateAsync(new IdentityRole(role));
+
+            var user = await _userManager.FindByNameAsync(username);
+            if (user == null)
+            {
+                var newUser = new ApplicationUser
+                {
+                    UserName = username,
+                    Email = email,
+                    FirstName = "Test",
+                    LastName = "User",
+                    EmailConfirmed = true
+                };
+
+                var result = await _userManager.CreateAsync(newUser, password);
+                if (result.Succeeded)
+                    await _userManager.AddToRoleAsync(newUser, role);
+            }
+        }
+    }
+}
---
+++ Million.TestUserCreator/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Million.Domain.Entities;
+using Million.Infrastructure.Persistence;
+using Million.TestUserCreator;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(context.Configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.AddTransient<IdentitySeeder>();
+    })
+    .Build();
+
+using var scope = host.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<IdentitySeeder>();
+await seeder.SeedAsync();
